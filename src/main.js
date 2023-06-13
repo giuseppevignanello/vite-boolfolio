4a38b36680f635a9dd4fd762412c94882f359ff3
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-13 17:31:46 +0200

    fix boostrap layout

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/scss/app.scss";
 
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-
-createApp(App).mount('#app')
+createApp(App).mount("#app");
