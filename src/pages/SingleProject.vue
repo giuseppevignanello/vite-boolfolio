7397aa945750bac950235f24237c317d4ddbcd59
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-14 17:36:50 +0200

    try to add single project view

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,16 +1,30 @@
 
 <script>
+
+import { store } from '../store'
 export default {
-    setup() {
-
-
-        return {}
+    name: 'SingleProject',
+    data() {
+        return {
+            slugParam: '',
+            store
+        }
+    },
+    mounted() {
+        this.slugParam = this.$route.params.slug
+        this.store.getProjects(this.store.apiUrl + this.store.ProjectsPath)
     }
 }
 </script>
 <template>
     <div>
-        Prova
+        <div class="card text-start">
+            <img class="card-img-top" src="" alt="Title">
+            <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Body</p>
+            </div>
+        </div>
     </div>
 </template>
 
