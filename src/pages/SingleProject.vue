5455592ed5f6f05202d046cd85e9789fe7258022
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-14 19:52:31 +0200

    fix single project view

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -8,14 +8,16 @@ export default {
     name: 'SingleProject',
     data() {
         return {
-
+            project: null,
             store
         }
     },
     mounted() {
-        axios.get(`${this.store.apiUrl}${this.store, ProjectsPath}${this.$route.params.slug}`).then((response) => {
+        axios.get(this.store.apiUrl + 'api/projects/' + this.$route.params.slug).then((response) => {
             if (response.data.success) {
+
                 this.project = response.data.project;
+                console.log(this.project);
             } else {
                 this.$router.push({ name: 'not-found' })
             }
@@ -26,7 +28,7 @@ export default {
 <template>
     <div>
         <div class="card text-start">
-            <img class="card-img-top" src="" alt="Title">
+            <img class="card-img-top" :src="this.store.apiUrl + 'storage/' + project.image" alt="Title">
             <div class="card-body">
                 <h4 class="card-title">{{ project.title }}</h4>
                 <p class="card-text">Body</p>
