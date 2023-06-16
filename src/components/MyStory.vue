c83edfd4b937d3600ccd339ec812defbd37933f2
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-16 17:43:25 +0200

    add about info

--- src/components/MyStory.vue
+++ src/components/MyStory.vue
@@ -13,28 +13,28 @@ export default {
 </script>
 
 <template>
-    <div class="p-5 mb-4 bg rounded-3">
-        <div class="container-fluid py-5">
+    <div class="container py-5">
+        <div>
             <h2 class="display-5 fw-bold">My Story</h2>
             <div class="col-md-8 fs-4">
-                <div class="d-flex justify-content-between text-white">
+                <div class="d-flex flex-column flex-sm-row justify-content-between text-white">
                     <div class="d-flex align-items-center">
-                        <h4>Sicily</h4>
+                        <h3 class:>Sicily</h3>
                         <img class="mb-2 ms-2" style="width: 30px" src=" ../../public/img/lemon.png" alt="lemon">
 
                     </div>
                     <div class="d-flex align-items-center">
-                        <h4>Bologna</h4>
+                        <h3>Bologna</h3>
                         <img class="mb-2 ms-2" style="width: 30px" src="../../public/img/tortellino.png" alt="tortellino">
 
                     </div>
                     <div class="d-flex align-items-center">
-                        <h4>Barcelona</h4>
+                        <h3>Barcelona</h3>
                         <img class="mb-2 ms-2" style="width: 30px" src="../../public/img/bravas.png" alt="bravas">
 
                     </div>
                 </div>
-                <div class="flight d-flex align-items-bottom">
+                <div class="flight align-items-bottom d-none d-sm-flex">
                     <div class="line me-2">
 
                     </div>
@@ -43,10 +43,22 @@ export default {
                     </div>
                 </div>
             </div>
-
-
         </div>
+        <div class="born">
+            <p>🚩 I was born in Sicily in 1997</p>
+        </div>
+        <div class="study">
+            <p>🚩 I studied philosophy at the University of Bologna <br>
+                and web development at Boolean Academy </p>
+        </div>
+        <div class="live">
+            <p>🚩I currently live in Barcelona </p>
+        </div>
+
+
+
     </div>
+
     <div class="container my-3">
 
 
