54de78506e9085c6844d6b08d454abbed53f724a
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-16 17:23:25 +0200

    add my story animation

--- src/components/MyStory.vue
+++ src/components/MyStory.vue
@@ -13,15 +13,42 @@ export default {
 </script>
 
 <template>
-    <div class="container my-3">
-        <div class="d-flex align-items-bottom">
-            <div class="line">
+    <div class="p-5 mb-4 bg rounded-3">
+        <div class="container-fluid py-5">
+            <h2 class="display-5 fw-bold">My Story</h2>
+            <div class="col-md-8 fs-4">
+                <div class="d-flex justify-content-between text-white">
+                    <div class="d-flex align-items-center">
+                        <h4>Sicily</h4>
+                        <img class="mb-2 ms-2" style="width: 30px" src=" ../../public/img/lemon.png" alt="lemon">
 
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <h4>Bologna</h4>
+                        <img class="mb-2 ms-2" style="width: 30px" src="../../public/img/tortellino.png" alt="tortellino">
+
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <h4>Barcelona</h4>
+                        <img class="mb-2 ms-2" style="width: 30px" src="../../public/img/bravas.png" alt="bravas">
+
+                    </div>
+                </div>
+                <div class="flight d-flex align-items-bottom">
+                    <div class="line me-2">
+
+                    </div>
+                    <div>
+                        <img style="width: 30px;" src="../../public/img/plane.png" alt="paper_airplane">
+                    </div>
+                </div>
             </div>
-            <div>
-                <i class="fa fa-plane fa-xl" aria-hidden="true"></i>
-            </div>
+
+
         </div>
+    </div>
+    <div class="container my-3">
+
 
     </div>
 </template>
