d3cfc993ccb215c81b62ab99e0a430732aee750b
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-20 16:30:11 +0200

    add select for projects

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -5,6 +5,7 @@ import AppLoader from "../components/AppLoader.vue"
 import ProjectCard from '../components/ProjectCard.vue'
 import PageButtons from "../components/PageButtons.vue"
 import JumboTron from "../components/JumboTron.vue"
+import axios from "axios"
 
 
 export default {
@@ -18,11 +19,27 @@ export default {
     data() {
         return {
             store,
+            types: [],
+            selectedType: "",
         };
+    },
+    methods: {
+        selectType() {
+            this.loading = true;
+            const url = this.store.apiUrl + this.store.ProjectsPath + '?type_id=' + this.selectedType;
+            this.store.getProjects(url);
+        }
     },
 
     mounted() {
         this.store.getProjects(this.store.apiUrl + this.store.ProjectsPath)
+
+        axios
+            .get(this.store.apiUrl + 'api/types')
+            .then((response) => {
+                this.types = response.data.types
+                console.log(this.types);
+            })
     },
 }
 </script>
@@ -32,13 +49,19 @@ export default {
     <div class="container">
         <h1 class="text-center mb-3 text-white"> <strong> MY PROJECTS </strong></h1>
         <div class="mb-5 w-25 m-auto">
-            <label for="types" class="form-label">Select type</label>
-            <select class="form-select" name="types" id="types">
-                <option selected>All</option>
-                <option value="">Front End</option>
-                <option value="">Back End</option>
-                <option value="">Full Stack</option>
-            </select>
+            <form action="" @change="selectType()">
+                <div class="d-flex gap-3">
+
+                    <div class="input">
+                        <label for="types" class="form-label">Select type</label>
+                        <select class="form-select" name="types" id="types" v-model="selectedType">
+                            <option value="">All</option>
+                            <option v-for="tipo in this.types" :value="tipo.id">{{ tipo.name }}</option>
+                        </select>
+                    </div>
+                </div>
+
+            </form>
         </div>
         <div class="row">
             <ProjectCard v-if="!this.store.loading" v-for="project in  this.store.projects " :title="project.title"
