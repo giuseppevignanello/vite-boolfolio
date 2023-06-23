bd8c5d809746b3dc47f5a36c9c11f4902dc1cad0
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-23 17:02:21 +0200

    fix jumbo text dimensions

--- src/components/JumboTron.vue
+++ src/components/JumboTron.vue
@@ -12,17 +12,19 @@ export default {
 <template>
     <div>
         <div class="p-5 mb-3 rounded-3">
-            <div class="container py-5 d-md-flex">
-                <div class="text w-md-50 ">
-                    <h1 class="display-5 fw-bold typewriter">Hi, I'm Peppe</h1>
-                    <p class="fs-5 typewriter">I'm an italian junior fullstack developer based in Barcelona
-                    </p>
-                    <router-link :to="{ name: 'about' }">
-                        <button class="btn bg_opacity btn-lg" type="button">About me</button>
-                    </router-link>
-                </div>
-                <div class="avatar bg_header my-2">
-                    <img class="me" src="../../public/img/me.png" alt="">
+            <div class="container py-5">
+                <div class="row">
+                    <div class="text col">
+                        <h1 class="display-5 fw-bold typewriter">Hi, I'm Peppe</h1>
+                        <p class="fs-3 typewriter">I'm an italian junior fullstack developer based in Barcelona
+                        </p>
+                        <router-link :to="{ name: 'about' }">
+                            <button class="btn bg_opacity btn-lg" type="button">About me</button>
+                        </router-link>
+                    </div>
+                    <div class="avatar bg_header my-2 col">
+                        <img class="me" src="../../public/img/me.png" alt="">
+                    </div>
                 </div>
             </div>
         </div>
