28019306db37c27776ad59e69635dc34352027b5
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-02 18:35:13 +0200

    add 404 page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -12,11 +12,20 @@ export default {
 
 <template>
     <div>
-        <h1>Not Found</h1>
+        <h1 class="text-center mt-2"> <strong>Oh no, you seem to be lost </strong></h1>
+        <h3 class="text-center">This is a 404 page actually</h3>
+        <p class="text-center"> I hope you find your way back home. Maybe the link below can help</p>
+        <div class="d-flex justify-content-center">
+            <div class="avatar_404 bg_header">
+                <img class="me-404" src="../../public/img/avatar_404.png" alt="">
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
 
-        <router-link :to="{ name: 'home' }">
-            Home
-        </router-link>
+            <router-link :to="{ name: 'home' }" class="text-center">
+                <h2 class="mt-3">Home</h2>
+            </router-link>
+        </div>
     </div>
 </template>
 
