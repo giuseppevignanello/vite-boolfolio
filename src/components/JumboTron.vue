cdae5c22971caadf1e69615dddea971df11af66b
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-08 11:49:05 +0200

    add typewriter animation

--- src/components/JumboTron.vue
+++ src/components/JumboTron.vue
@@ -5,7 +5,8 @@ export default {
 
 
         return {}
-    }
+    },
+
 }
 </script>
 
@@ -14,16 +15,33 @@ export default {
         <div class="p-5 mb-3 rounded-3">
             <div class="container py-5">
                 <div class="row">
-                    <div class="text col-12 col-md-6">
-                        <h1 class="display-5 fw-bold typewriter">Hi, I'm Peppe</h1>
-                        <p class="fs-3 typewriter">I'm an italian junior fullstack developer based in Barcelona
+                    <div class="text col-12 col-lg-6">
+                        <h1 class="display-5 fw-bold">Hi, I'm Peppe</h1>
+                        <p class="fs-3">I'm an italian junior fullstack developer based in Barcelona
                         </p>
                         <router-link :to="{ name: 'about' }">
                             <button class="btn bg_opacity btn-lg" type="button">About me</button>
                         </router-link>
                     </div>
-                    <div class="avatar bg_header my-2 col-12 col-md-6">
-                        <img class="me" src="../../public/img/me.png" alt="">
+                    <div class="avatar bg_header my-3 col-12 col-lg-6 d-flex">
+                        <img class="me col-sm-6 col-md-7 d-none col-lg-6 d-sm-block" src="../../public/img/me.png" alt="">
+                        <div class="text-white d-flex align-items-center">
+                            <p class="code_text my-3">
+                                <span class="first_line"><span class="text-primary"> let</span>
+                                    meHappy = <span class="text-primary">false</span>; </span>
+                                <br>
+                                <span class="second_line"> <span class="text-primary">let</span> hasBooks = <span
+                                        class="text-primary">true</span>; </span>
+                                <br>
+                                <span class="third_line"> <span class="text-primary">let</span> hasCode = <span
+                                        class="text-primary">true</span>; </span><br>
+                                <span class="fourth_line"> <span class="text-primary">if</span> (hasBooks && hasCode) {
+                                </span><br>
+                                <span class="fifth_line"> &nbsp &nbsp meHappy = <span class="text-primary">true</span>
+                                </span><br>
+                                <span class="sixth_line">} </span>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
