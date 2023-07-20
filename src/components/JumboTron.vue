930b41358fb609a0694436f75f9b57378462c9ba
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-20 13:36:25 +0200

    add tech stack on home

--- src/components/JumboTron.vue
+++ src/components/JumboTron.vue
@@ -1,11 +1,33 @@
 
 <script>
+import { store } from '../store';
+import axios from 'axios';
 export default {
     data() {
 
 
-        return {}
-    },
+        return {
+            store,
+            TechnologiesPath: "api/technologies",
+            technologies: [],
+            loading: true,
+        }
+    }, methods: {
+        getTechnologies(url) {
+            axios
+                .get(url)
+                .then((response) => {
+                    this.loading = false;
+                    this.technologies = response.data.technologies
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    }, mounted() {
+        this.getTechnologies(this.store.apiUrl + this.TechnologiesPath)
+
+    }
 
 }
 </script>
@@ -22,8 +44,9 @@ export default {
                         <router-link :to="{ name: 'about' }">
                             <button class="btn bg_opacity btn-lg" type="button">About me</button>
                         </router-link>
+
                     </div>
-                    <div class="avatar bg_header my-3 col-12 col-lg-6 d-flex">
+                    <div class="avatar bg_header my-3 col-12 col-lg-6 d-flex h-50">
                         <img class="me col-sm-6 col-md-7 d-none col-lg-6 d-sm-block" src="../../public/img/me.png" alt="">
                         <div class="text-white d-flex align-items-center">
                             <p class="code_text my-3">
@@ -43,6 +66,18 @@ export default {
                             </p>
                         </div>
                     </div>
+                </div>
+                <div class="card p-3 shadow bg_opacity mt-3">
+                    <h2 class="fw-bold">My tech stack</h2>
+                    <ul class="d-flex list-unstyled flex-wrap justify-content-center">
+                        <li class="technology my-3" v-for="technology in this.technologies">
+                            <div class="me-5 tech">
+                                <img class="text-center" style="width: 100px; height: 100px;"
+                                    :src="'https://api.giuseppevignanello.com/storage/' + technology.image" alt="">
+                                <p class="text-center"> <strong>{{ technology.name }} </strong> </p>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
